5efe7674d05f013b7fd42bdbbfe1722f6042f9bf
Marco <marcoaraujo.grossi@gmail.com>
2023-11-24 22:58:47 -0300

    LPW - Atividades 2023

--- POKESNAP 2/script/balaozinho-jquery.js
+++ POKESNAP 2/script/balaozinho-jquery.js
@@ -1,2 +1,42 @@
-// Sugestão: mantenha este arquivo e o balaozinho-vanilla.js abertos
-//           lado a lado e vá "traduzindo" o código para usar jQuery
+let $marcacoes = $('.marcacao')
+let $balaozinho = $('#balaozinho')
+
+const alteraBalaozinho = (e) => {
+    let $marcacaoAtual = $(e.currentTarget)
+
+    let $titulo = $marcacaoAtual.data('titulo')
+    let $conteudo = $marcacaoAtual.data('conteudo')
+
+    $balaozinho.html(`<h2>${$titulo}</h2><p>${$conteudo}</p>`)
+} 
+
+const voltaBalaozinho = () => {
+    $balaozinho.html('')
+}
+
+const segueMouse = (e) => {
+    $balaozinho.css('left', `${e.pageX}px`)
+    $balaozinho.css('top', `${e.pageY}px`)
+}
+
+$marcacoes.mouseover(alteraBalaozinho)
+$marcacoes.mouseout(voltaBalaozinho)
+$marcacoes.mousemove(segueMouse)
+
+const alteraMarcacao = () => {
+    let $x = $('#marcacao-x').val()
+    let $y = $('#marcacao-y').val()
+    let $largura = $('#marcacao-largura').val()
+    let $altura = $('#marcacao-altura').val()
+
+    $($marcacoes[0]).css('left', `${$x}px`)
+    $($marcacoes[0]).css('top', `${$y}px`)
+    $($marcacoes[0]).css('width', `${$largura}px`)
+    $($marcacoes[0]).css('height', `${$altura}px`)
+}
+
+let $botaoDefinirMarcacao = $('#definir-marcacao')
+$botaoDefinirMarcacao.click(alteraMarcacao)
+
+let $inputs = $('input')
+$inputs.change(alteraMarcacao)
